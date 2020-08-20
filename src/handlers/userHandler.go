f1d7b97bc0f45d59afd2643c525d1830a3f173fc
youssef <youss.hark@gmail.com>
2020-08-20 18:07:18 -0400

    Added post user in users route

--- src/handlers/userHandler.go
+++ src/handlers/userHandler.go
@@ -2,8 +2,26 @@ package handlers
 
 import (
 	"GoAPI/src/user"
+	"encoding/json"
+	"errors"
+	"gopkg.in/mgo.v2/bson"
+	"io/ioutil"
 	"net/http"
 )
+
+func bodyToUser(r *http.Request, u *user.User) error {
+	if r.Body == nil {
+		return errors.New("Request body is empty")
+	}
+	if u == nil {
+		return errors.New("A user is required")
+	}
+	bd, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(bd, u)
+}
 
 func usersGetAll(w http.ResponseWriter, r *http.Request)  {
 	users, err := user.All()
@@ -12,4 +30,25 @@ func usersGetAll(w http.ResponseWriter, r *http.Request)  {
 		return
 	}
 	postBodyResponse(w, http.StatusOK, jsonResponse{"users": users})
+}
+
+func usersPostOne(w http.ResponseWriter, r *http.Request) {
+	u := new(user.User)
+	err := bodyToUser(r, u)
+	if err != nil {
+		postError(w, http.StatusBadRequest)
+		return
+	}
+	u.ID = bson.NewObjectId()
+	err = u.Save()
+	if err != nil {
+		if err == user.ErrRecordInvalid {
+			postError(w, http.StatusBadRequest)
+		}else {
+			postError(w, http.StatusInternalServerError)
+		}
+		return
+	}
+	w.Header().Set("Location", "/users/"+u.ID.Hex())
+	w.WriteHeader(http.StatusCreated)
 }
